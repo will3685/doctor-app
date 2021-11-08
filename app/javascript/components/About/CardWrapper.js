f222e432398714acc8cd98daa4feb37fded74976
Will Theard <lavardwilltheard@yahoo.com>
2021-11-08 13:58:01 -0300

    remove card wrapper

---
+++ app/javascript/components/About/CardWrapper.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const CardWrapper = () => {
+  return (
+    <div>
+      King
+    </div>
+  )
+}
+
+/* <Card 
+          style={{ height: "120vh"  }}>
+          <Card.Grid 
+            hoverable={false} 
+            style={{ width: '50%', height: "80vh", alignItems:"center", textAlign: "justify" }}>
+            <h1>Dr. Fleury Kwegir Johnson</h1>
+            <h5>CRM 52-01138707</h5>
+            <Divider />
+            <p style={{ fontSize: 18, textIndent: 15}}>Nascido no Togo (África), Fleury Johnson inicia sua trajetória no âmbito da saúde ainda no país de origem. <br />
+
+Na adolescência, participou de treinamentos para educação popular quanto às Infecções Sexualmente Transmissíveis, colaborando especialmente na conscientização contra o HIV.
+
+Posteriormente, ingressou na faculdade de medicina de sua cidade, até optar pela oportunidade de realizar seus estudos em país estrangeiro, visto que sempre sonhou em se formar em um novo local que também pudesse o enriquecer culturalmente e potencializar seu futuro trabalho. <br />
+
+Após aprovação em alguns concursos para diferentes países, optou pelo Brasil, por ser um lugar com características sociais semelhantes ao seu país natal e por vislumbrar aqui importantes oportunidades.
+
+Já em terras brasileiras, precisamente no Rio de Janeiro, inicia sua jornada acadêmica em uma das melhores universidades do Brasil: a Universidade Federal do Rio de Janeiro (UFRJ), onde obteve seu diploma de graduação em 2018.
+
+De 2018 a 2019, aprofundou sua prática como médico plantonista em diversas unidades de saúde, entre públicas e privadas, acumulando experiência e compreensão sobre variados públicos e suas demandas. <br /> 
+
+Em 2020, inicia como especializando em Clínica Médica pelo hospital Pasteur, como pré-requisito de seu próximo objetivo: especializar-se em cardiologia.
+
+Em sua trajetória, desde a época da graduação, tem se dedicado ao campo da Saúde da População Negra, sendo um agente de promoção da Política Nacional Integral de Saúde da População Negra. Por este compromisso, ministra aulas em diversas faculdades de medicina e outros espaços sobre antirracismo no contexto de práticas médicas e gestão em saúde, fomentando a construção de um acesso à saúde com equidade.<br />
+
+Desde 2021, ainda expandindo seu perfil enquanto médico educador, atua como professor de Clínica Médica no curso VerboMed. 
+
+Por sua história pessoal de desafios e perseverança no sonho de ser médico, bem como por sua postura frente às questões raciais e sociais, foi indicado ao prêmio Faz Diferença do Jornal O Globo (2018), foi convidado ao programa Encontro com Fátima Bernades da mesma emissora, além de conceder diversas entrevistas e participações em múltiplas plataformas de comunicação.</p>
+          </Card.Grid >
+          <Card.Grid 
+            hoverable={false}
+            style={{ width: '50%', height: "120vh", backgroundColor: 'gray', filter: "grayscale(100%)", backgroundImage: `url(${about})`}}
+            ></Card.Grid>
+        </Card> */
